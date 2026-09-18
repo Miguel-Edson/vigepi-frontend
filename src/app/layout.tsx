0a05e177ca06cce85b387246e99aab5bd1dee841
Miguel-Edson <migueledson.11@gmail.com>
2026-09-18 10:19:37 -0300

    feat: adiciona logo do simos e atualiza layout global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/layout/NavBar";
 
 const roboto = Roboto({
   weight: ["400", "500", "700", "900"],
@@ -20,8 +21,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${roboto.variable} antialiased`}>
-        {children}
+      <body className={`${roboto.variable} font-sans antialiased bg-gray-50 min-h-screen flex flex-col`}>
+        {/* Barra de Navegação Superior Fixa para todo o sistema */}
+        <Navbar />
+        
+        {/* Conteúdo dinâmico das páginas */}
+        <main className="flex-1">
+          {children}
+        </main>
       </body>
     </html>
   );
